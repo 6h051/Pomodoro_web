39e526c270fa6452b38d41ff5c62914a82a5330d
6h051 <nicosavemind1@protonmail.com>
2022-04-04 16:20:10 -0500

    Update if

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,9 @@
-function live()
-{
+function live() {
     let a = document.getElementById("entrada").value;
     a = parseInt(a);
-    document.getElementById("title").innerHTML = 2022 - a;
 
+    if (a > 0)
+    {
+        document.getElementById("title").innerHTML = 2022 - a;
+    }
 }
